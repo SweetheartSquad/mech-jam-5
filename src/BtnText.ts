55c49767a9a2e4d7f231dbef4731c6aa5d27706e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-07-11 22:45:51 -0400

    expose text

--- src/BtnText.ts
+++ src/BtnText.ts
@@ -3,16 +3,17 @@ import { Btn } from './Btn';
 import { fontDialogue } from './font';
 
 export class BtnText extends Btn {
+	text: BitmapText;
 	constructor(
 		str: string,
 		onClick: ConstructorParameters<typeof Btn>[0],
 		title?: ConstructorParameters<typeof Btn>[2]
 	) {
 		super(onClick, 'button', title);
-		const text = new BitmapText({ text: str, style: fontDialogue });
-		this.display.container.addChild(text);
-		text.x -= text.width / 2;
-		text.y -= text.height / 2;
-		text.x += 4;
+		this.text = new BitmapText({ text: str, style: fontDialogue });
+		this.display.container.addChild(this.text);
+		this.text.x -= this.text.width / 2;
+		this.text.y -= this.text.height / 2;
+		this.text.x += 4;
 	}
 }
