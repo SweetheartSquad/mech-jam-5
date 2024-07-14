33751dbd5d5d2b036de02dff2d5263175d2d8148
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-07-13 20:17:36 -0400

    half tints

--- src/tints.ts
+++ src/tints.ts
@@ -2,5 +2,7 @@ export const white = 0xffffff;
 export const gray = 0x999999;
 export const black = 0x000000;
 export const red = 0xff0000;
+export const redHalf = 0xff6666;
 export const green = 0x00ff00;
+export const greenHalf = 0x66ff66;
 export const blue = 0x0000ff;
