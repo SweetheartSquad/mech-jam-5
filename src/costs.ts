aa4a31ec4aea9ee096f17ed5a38dee0353d32ea5
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-07-14 17:09:27 -0400

    tweak costs

--- src/costs.ts
+++ src/costs.ts
@@ -7,12 +7,12 @@ export const costPerJoint = 9;
 
 // modules
 export const costPerModuleCell = -3;
-export const costPerCockpitCell = 100;
+export const costPerCockpitCell = 50;
 export const costPerArmourCell = 8;
-export const costPerJointExtendCell = 100;
+export const costPerJointExtendCell = 50;
 export const costPerAttack = 100;
-export const costPerRadar = 100;
-export const costPerShield = 100;
-export const costPerHeatsink = 100;
+export const costPerRadar = 75;
+export const costPerShield = 50;
+export const costPerHeatsink = 75;
 export const costMultAwkward = 0.8;
 export const costMultFlexible = 1.5;
