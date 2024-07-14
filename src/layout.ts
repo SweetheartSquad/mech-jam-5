9d1f5e91070d8b9ef7bbc8e21d6c568797f0ca9e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-07-14 19:41:47 -0400

    fix keyxy

--- src/layout.ts
+++ src/layout.ts
@@ -189,5 +189,5 @@ export function xyKey(x: number, y: number) {
 	return `${x},${y}`;
 }
 export function keyXY(key: string) {
-	return key.split(',').map((i) => parseInt(i, 10)) as [number, number];
+	return key.split(',').map((i) => Number(i)) as [number, number];
 }
